fe319d6886d6441f0e91cbebee69397633d217e7
FunctionUP-Teju <kapilsharma68@gmail.com>
2023-08-30 23:39:26 +0530

    again routes update

--- routes/routes.js
+++ routes/routes.js
@@ -32,7 +32,7 @@ mongoose.connect(process.env.DB).then(()=>{
   console.log("Not connected, try later");
 })   
 
-app.use(express.static(path.join(__dirname, "./frontend/build")))
+app.use(express.static(path.join(__dirname, "frontend/build")))
 // "proxy": "http://localhost:4000/",
 app.get("*" , function(req,res){
   const index = path.join(__dirname, "build" ,"index.html")
